b912cbfca5e67e3373fb1bf43ce5f9fefdf7d455
Jarosław Maleszyk <jaroslaw.maleszyk@orlen.pl>
2026-02-04 13:43:07 +0100

    ...

--- answers/models/staging/stg_ecommerce__events_microbatch.sql
+++ answers/models/staging/stg_ecommerce__events_microbatch.sql
@@ -1,42 +1,36 @@
 {{
-	config(
-		materialized='incremental',
-        incremental_strategy='microbatch',
-        event_time="created_at",
-        batch_size="day",
-        lookback=2,
-        begin='2025-01-01',
-		on_schema_change='sync_all_columns',
-		partition_by={
-			"field": "created_at",
-			"data_type": "timestamp",
-			"granularity": "day"
-		}
-	)
+    config(
+        materialized='incremental',
+        unique_key='event_id',
+        on_schema_change='sync_all_columns'
+    )
 }}
 
 WITH source AS (
-	SELECT *
-
-	FROM {{ source('thelook_ecommerce', 'events') }}
+    SELECT *
+    FROM {{ source('thelook_ecommerce', 'events') }}
+    {% if is_incremental() %}
+      -- Filtrowanie, aby dbt nie przeliczał wszystkiego od nowa
+      WHERE created_at > (SELECT max(created_at) FROM {{ this }})
+    {% endif %}
 )
 
 SELECT
-	id AS event_id,
-	user_id,
-	sequence_number,
-	session_id,
-	created_at,
-	ip_address,
-	city,
-	state,
-	postal_code,
-	browser,
-	traffic_source,
-	uri AS web_link,
-	event_type,
-	{# Look in macros/macro_get_brand_name.sql to see how this function is defined #}
-	{{ target.schema }}.get_brand_name(uri) AS brand_name
-
+    id AS event_id,
+    user_id,
+    sequence_number,
+    session_id,
+    created_at,
+    ip_address,
+    city,
+    state,
+    postal_code,
+    browser,
+    traffic_source,
+    uri AS web_link,
+    event_type,
+    -- Jeśli funkcja get_brand_name nie jest zarejestrowana w DuckDB, 
+    -- to tutaj też może wystąpić błąd. Na czas testu możesz to zakomentować.
+    uri AS brand_name 
 
 FROM source
